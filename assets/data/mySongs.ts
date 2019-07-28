df2ff273537f3b84bdedf1f630d35478a472a49b
froggattsheridan <52541455+froggattsheridan@users.noreply.github.com>
2019-07-28 16:12:46 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,32 @@
+import { Songs } from "../../app/Songs";
+
+export const MYSONGS: Songs[] = [
+  {
+    name: "My Way",
+    artist: "The Sex Pistols",
+    genre: "Punk",
+    released: 1979,
+    picture: "./assets/images/myWay.jfif"
+  },
+  {
+    name: "One of These Days",
+    artist: "Pink Floyd",
+    genre: "Progressive Rock",
+    released: 1971,
+    picture: "./assets/images/oneOfTheseDays.jfif"
+  },
+  {
+    name: "Begin the Begin",
+    artist: "R. E. M.",
+    genre: "Alternative/Indie",
+    released: 1986,
+    picture: "./assets/images/beginTheBegin.jfif"
+  },
+  {
+    name: "Fell In Love With A Girl",
+    artist: "The White Stripes",
+    genre: "Alternative/Indie",
+    released: 2001,
+    picture: "./assets/images/fellInLove.jfif"
+  }
+];
